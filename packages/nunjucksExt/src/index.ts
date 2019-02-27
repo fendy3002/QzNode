c37a04bbd605eb704efb7958d63df39be7c8e135
Fendy Heryanto <fendy3002@yahoo.com>
2019-02-27 21:39:11 +0700

    Added index

---
+++ packages/nunjucksExt/src/index.ts
@@ -0,0 +1,20 @@
+import * as libRender from "./lib/render";
+
+export = {
+    filter: {
+        bool: require('./filter/bool'),
+        currency: require('./filter/currency'),
+        date: require('./filter/date'),
+        datetime: require('./filter/datetime'),
+        hasVal: require('./filter/hasVal'),
+        json: require('./filter/json'),
+        pagination: require('./filter/pagination'),
+        queryString: require('./filter/queryString')
+    },
+    global: {
+        render: require('./global/render')
+    },
+    lib: {
+        render: libRender
+    }
+}

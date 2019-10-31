dd37aac91cf5cf74210e4a8bfd5713502c29c024
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2019-10-31 13:22:57 +0700

    add rethrow

--- packages/expressHelper/src/errorHandler/index.ts
+++ packages/expressHelper/src/errorHandler/index.ts
@@ -1,2 +1,5 @@
 export {default as apiErrorHandler} from './apiErrorHandler';
 export {default as handled} from './handled';
+
+import * as rethrow from './rethrow';
+export {rethrow};
---
+++ packages/expressHelper/src/errorHandler/rethrow.ts
@@ -0,0 +1,10 @@
+export const append = (innerErr, newErr) => {
+    newErr.original = innerErr;
+    newErr.stack = newErr.stack.split('\n').slice(0, 2).join('\n') + '\n' +
+        innerErr.stack;
+    return newErr;
+};
+export const from = (innerErr) => (message) => {
+    let newErr = new Error(message);
+    return append(innerErr, newErr);
+};

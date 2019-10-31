687340e3055c2bf33d77922e358b4fd311ba9055
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2019-10-31 14:02:41 +0700

    change rethrow

--- packages/expressHelper/src/errorHandler/rethrow.ts
+++ packages/expressHelper/src/errorHandler/rethrow.ts
@@ -1,12 +1,19 @@
 export const append = (innerErr, newErr) => {
     newErr.original = innerErr;
-    if(innerErr.stack){
+    if (innerErr.stack) {
         newErr.stack = newErr.stack.split('\n').slice(0, 2).join('\n') + '\n' +
             innerErr.stack;
     }
     return newErr;
 };
-export const from = (innerErr) => (message) => {
-    let newErr = new Error(message);
-    return append(innerErr, newErr);
+export const from = (innerErr) => {
+    return {
+        error: (err) => {
+            return append(innerErr, err);
+        },
+        message: (message) => {
+            let newErr = new Error(message);
+            return append(innerErr, newErr);
+        }
+    };
 };

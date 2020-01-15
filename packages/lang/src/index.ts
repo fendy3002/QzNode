94c1d417711f72e84b87876dc766d86265f0b152
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2020-01-15 10:36:58 +0700

    Added markdown render option

--- packages/lang/src/index.ts
+++ packages/lang/src/index.ts
@@ -1,42 +1,54 @@
 import lo = require('lodash');
+import markdown = require('markdown-it');
 import * as myType from './types';
-export let pack: myType.PackConstructor = (langDictionary: myType.Lang.Content) => {
-    let _ = (path: string, ifNull ?: string, params ?: myType.Lang.UseParams) => {
-        let content = lo.get(langDictionary, path, ifNull) as string;
-        if(!params){
-            return content;
+export let pack: myType.PackConstructor = (langDictionary: myType.Lang.Content, context: myType.Context) => {
+    let getter = (val) => {
+        if (context.render == "markdown") {
+            return markdown.render(val);
         }
-        for(let key of Object.keys(params)){
+        else {
+            return val;
+        }
+    }
+    let _ = (path: string, ifNull?: string, params?: myType.Lang.UseParams) => {
+        let content = lo.get(langDictionary, path, ifNull) as string;
+        if (!params) {
+            return getter(content);
+        }
+        for (let key of Object.keys(params)) {
             let regexPattern = new RegExp("{" + key + "}", "g");
             content = content.replace(regexPattern, params[key]);
         }
-        return content;
+        return getter(content);
     };
     return {
         _: _,
         get: _,
         part: (path: string) => {
-            return pack(lo.get(langDictionary, path) as myType.Lang.Content);
+            return pack(lo.get(langDictionary, path) as myType.Lang.Content, context);
         }
     };
 };
-export let core: myType.CoreConstructor = async(initDictionary: myType.Lang.Dictionary) => {
+export let core: myType.CoreConstructor = async (initDictionary: myType.Lang.Dictionary, option: myType.Option) => {
     let langDictionary: myType.Lang.Dictionary = {
         ...initDictionary
     };
+    let context: myType.Context = lo.merge({
+        render: "default"
+    }, option);
     let coreInstance: myType.Core = {
         addLang: (languagecode: string, content: myType.Lang.Content) => {
             langDictionary[languagecode] = lo.merge(langDictionary[languagecode], content);
             return coreInstance;
         },
         use: (languagecode: string) => {
-            return pack(langDictionary[languagecode]);
+            return pack(langDictionary[languagecode], context);
         }
     };
 
     return coreInstance;
 };
 
-export {myType as type};
+export { myType as type };
 
 export default core;
--- packages/lang/src/types.ts
+++ packages/lang/src/types.ts
@@ -2,26 +2,33 @@ export namespace Lang {
     export interface Content {
         [key: string]: string | Content
     };
-    export interface Dictionary{
+    export interface Dictionary {
         [languagecode: string]: Content
     };
-    export interface UseParams{
+    export interface UseParams {
         [key: string]: string
     };
 }
 
-export interface CoreConstructor{
-    (initDictionary: Lang.Dictionary): Promise<Core>
+export interface Option {
+    render?: string
 };
-export interface Core{
+export interface Context {
+    render: string
+};
+
+export interface CoreConstructor {
+    (initDictionary: Lang.Dictionary, option?: Option): Promise<Core>
+};
+export interface Core {
     addLang: (languagecode: string, content: Lang.Content) => Core
     use: (languagecode: string) => Pack
 };
 export interface PackConstructor {
-    (langDictionary: Lang.Content): Pack
+    (langDictionary: Lang.Content, context: Context): Pack
 };
-export interface Pack{
-    _: (path: string, ifNull ?: string, params ?: Lang.UseParams) => string
-    get: (path: string, ifNull ?: string, params ?: Lang.UseParams) => string
+export interface Pack {
+    _: (path: string, ifNull?: string, params?: Lang.UseParams) => string
+    get: (path: string, ifNull?: string, params?: Lang.UseParams) => string
     part: (path: string) => Pack
 };

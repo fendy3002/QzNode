6296bab2cad3193f0ce2b533a36b45525d723ead
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2020-03-10 10:08:30 +0700

    dev

--- packages/mongooseMigration/src/migrateService.ts
+++ packages/mongooseMigration/src/migrateService.ts
@@ -1,5 +1,6 @@
 import fs = require('fs');
 import path = require('path');
+import lo = require('lodash');
 import * as types from './types';
 import padVersion from './padVersion';
 import schemaVersionModelConstructor from './schemaVersionModel';
@@ -19,12 +20,32 @@ let service: types.MigrateServiceConstructor<any> = (config: types.MigrateConfig
     return {
         migrate: async () => {
             let schemaVersionModel = await schemaVersionModelConstructor(config);
-            let migrationFiles = fs.readdirSync(config.dirpath);
+            let migrationFiles = lo.orderBy(
+                fs.readdirSync(config.dirpath)
+            );
+            let maxStoredVersionRecord = (await schemaVersionModel.aggregate([
+                {
+                    $project: {
+                        maxVersion: {
+                            $max: "$_id"
+                        }
+                    }
+                }
+            ]))[0];
             for (let migrationFile of migrationFiles) {
+                let fullPath = path.join(config.dirpath, migrationFile);
                 let fileVersion = path.basename(migrationFile).split("__")[0];
                 fileVersion = fileVersion.replace("V", "");
                 let paddedVersion = padVersion(fileVersion);
-                
+                if (paddedVersion < maxStoredVersionRecord.maxVersion) {
+                    let migrateRequest: types.MigrateRequest<any> = (await import(fullPath)).default;
+                    await migrateRequest.migrate({
+                        ...config,
+                        version: paddedVersion,
+                        schemaVersionModel: schemaVersionModel,
+                        updateModelVersion: updateModelVersion
+                    });
+                }
             }
         }
     }

09c41c480640892780522067a94b65f16c168ffe
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2020-03-11 10:28:55 +0700

    index

--- packages/expressHelper/src/index.ts
+++ packages/expressHelper/src/index.ts
@@ -1,8 +1,10 @@
 import * as errorHandler from './errorHandler';
 import * as filterParser from './filterParser';
+import * as sortParser from './sortParser';
 import * as validator from './validator';
 
 export { default as healthcheck } from './healthcheck';
 export { errorHandler };
 export { filterParser };
+export { sortParser };
 export { validator };

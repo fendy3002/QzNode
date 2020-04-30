8d3d9075bee318fedee42cb4c234294cf7d13c21
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2020-04-30 09:34:07 +0700

    add retryable, push minor version

--- packages/expressHelper/src/index.ts
+++ packages/expressHelper/src/index.ts
@@ -2,9 +2,11 @@ import * as errorHandler from './errorHandler';
 import * as filterParser from './filterParser';
 import * as sortParser from './sortParser';
 import * as validator from './validator';
+import * as retryable from './retryable';
 
 export { default as healthcheck } from './healthcheck';
 export { errorHandler };
 export { filterParser };
 export { sortParser };
 export { validator };
+export { retryable };
---
+++ packages/expressHelper/src/retryable/index.ts
@@ -0,0 +1,39 @@
+import * as rethrow from '../errorHandler/rethrow';
+interface Handle {
+    (): Promise<void | any>
+}
+const retryable = (handle: Handle) => {
+    const times = async (number: number) => {
+        let tryingTimes = 0;
+        let throwEx = null;
+        while (tryingTimes <= number) {
+            try {
+                await handle();
+                return;
+            } catch (ex) {
+                throwEx = ex;
+                tryingTimes++;
+            }
+        }
+        throw rethrow.from(throwEx).message(`Error after retrying for ${number} times`);
+    };
+    const forLong = async (duration: number) => {
+        const tsNow = new Date().getTime();
+        let throwEx = null;
+        while (new Date().getTime() - tsNow <= duration) {
+            try {
+                await handle();
+                return;
+            } catch (ex) {
+                throwEx = ex;
+            }
+        }
+        throw rethrow.from(throwEx).message(`Error after retrying for ${(duration / 1000).toFixed(2)} second(s)`);
+    };
+    return {
+        times,
+        forLong
+    }
+};
+
+export default retryable;

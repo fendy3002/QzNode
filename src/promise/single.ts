ace3c7e2cb72bb8e5e3d791fe18461c2dad62df3
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2020-07-02 11:52:13 +0700

    dev

--- src/promise/single.ts
+++ src/promise/single.ts
@@ -7,25 +7,28 @@ import * as qzPromise from './index';
 import * as types from '../types';
 
 class SinglePromise {
-    constructor(option, handle) {
+    constructor(option: types.Qz.Promise.SingleOption, handler) {
         this.option = option;
-        this.handle = handle;
+        this.handler = handler;
     }
     option: types.Qz.Promise.SingleOption;
-    handle;
+    handler;
     delay = 0;
     timeout = 0;
     retry = {
         type: null,
-        amount: 0
+        amount: 0,
+        option: null
     };
     locks = [];
 
     withDelay(delay) {
         this.delay = delay;
+        return this;
     }
     withTimeout(timeout) {
         this.timeout = timeout;
+        return this;
     }
     withRetry(amount, type) {
         if (![
@@ -39,6 +42,7 @@ class SinglePromise {
         }
         this.retry.type = type;
         this.retry.amount = amount;
+        return this;
     }
     addLock(key) {
         if (Array.isArray(key)) {
@@ -47,9 +51,11 @@ class SinglePromise {
         else {
             this.locks.push(key);
         }
+        return this;
     }
     clearLock() {
         this.locks = [];
+        return this;
     }
     async exec() {
         if (this.delay > 0) {
@@ -60,54 +66,76 @@ class SinglePromise {
             })
         }
 
+        let timeoutHandle = () => {
+            if (this.delay > 0) {
+                return Promise.race([
+                    this.handler(),
+                    new Promise(function (resolve, reject) {
+                        setTimeout(function () {
+                            reject('Timed out');
+                        }, this.delay);
+                    })
+                ]);
+            } else {
+                return this.handler();
+            }
+        };
         let lockHandle = async () => {
             let locks = [];
             for (let key of lo.sortBy(this.locks)) {
                 let lock = await this.option.lockEngine.lock(key, this.option.lockTTL);
                 locks.push(lock);
             }
-
+            try {
+                await timeoutHandle();
+            } finally {
+                for (let lock of locks) {
+                    await lock.unlock();
+                }
+                if (this.locks.length > 0) {
+                    debug(this.locks.join(",") + " released");
+                }
+            }
         };
+        let retryHandle = async () => {
+            if (this.retry.type == "times") {
+                return await qzPromise.retryable(lockHandle).times(this.retry.amount, this.retry.option);
+            }
+            else if (this.retry.type == "ms") {
+                return await qzPromise.retryable(lockHandle).times(this.retry.amount, this.retry.option);
+
+            }
+            else if (!this.retry.type) {
+                return lockHandle();
+            }
+        };
+        return retryHandle();
     }
 }
 
-const singleFactory = async (option) => {
-    let context = {
-        engine: await memoryEngine()
+const singleFactory = async () => {
+    let defaultContext = {
+        lockEngine: await memoryEngine(),
+        lockTTL: 30000
     };
-    let handle = (handler) => {
-        let context = {
-            delay: 0,
-            timeout: 0,
-            retry: null,
+    let handle = (context) => (handler) => {
+        return new SinglePromise(context, handler);
+    };
 
-        };
-
-        let withDelay = (delay) => {
-            context.delay = delay;
-
-        }
-        let withRetry = (amount, type) => {
-
-        };
-        let withTimeout = (timeout: number) => {
-
-        };
-        let withLock = (key) => {
-
-        };
-        let clearLock = () => {
-
-        };
-        let exec = async () => {
-
+    let withEngine = (context) => (engine: types.Qz.Promise.LockEngine, ttl: number) => {
+        let newContext = {
+            ...context,
+            lockEngine: engine,
+            lockTTL: ttl
         };
         return {
-            exec
+            handle: handle(newContext),
+            withEngine: withEngine(newContext)
         };
     };
     return {
-        handle
+        withEngine: withEngine(defaultContext),
+        handle: handle(defaultContext)
     };
 }
 

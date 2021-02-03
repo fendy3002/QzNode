b949ddc4e5e78718ac2f98a57499e3781411c508
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-02-03 16:10:36 +0700

    fix export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import array = require('./array');
 import date = require("./date");
 import math = require("./math");
 import error = require("./error");
-import excel = require("./excel");
+import excel from "./excel";
 import json = require("./json");
 import string = require("./string");
 import promise = require("./promise");

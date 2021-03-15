63010036919fc9eb6ee25e99013b9766798a1dd1
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-03-15 17:57:10 +0700

    dev

--- packages/codeGenerator/src/helper.ts
+++ packages/codeGenerator/src/helper.ts
@@ -1,22 +1,25 @@
 import path = require('path');
 import fs = require('fs');
+import { error } from '@fendy3002/qz-node';
 import * as types from './types';
 
 import nunjucks = require('nunjucks');
 const getHelper = async (context: types.Context) => {
     const extensions: any = {};
-    for (let extensionFile of fs.readdirSync(context.path.extension)) {
-        let extensionFullPath = path.join(context.path.extension, extensionFile);
-        let extensionRaw = (await import(extensionFullPath)).default;
-        let extension = await extensionRaw(context.schema);
-        let extensionName = "";
-        if (!extension.name) {
-            extensionName = path.basename(extensionFile);
+    if (fs.existsSync(context.path.extension)) {
+        for (let extensionFile of fs.readdirSync(context.path.extension)) {
+            let extensionFullPath = path.join(context.path.extension, extensionFile);
+            let extensionRaw = (await import(extensionFullPath)).default;
+            let extension = await extensionRaw(context.schema);
+            let extensionName = "";
+            if (!extension.name) {
+                extensionName = path.basename(extensionFile);
+            }
+            else {
+                extensionName = extension.name;
+            }
+            extensions["_" + extensionName] = extension.data;
         }
-        else {
-            extensionName = extension.name;
-        }
-        extensions["_" + extensionName] = extension.data;
     }
 
     const renderHelper = (helperName: string, data: any) => {
@@ -25,11 +28,15 @@ const getHelper = async (context: types.Context) => {
         if (path.extname(replacedHelperName) == ".html") {
             renderer = context.nunjucks.html;
         }
-        return renderer.render(path.join(context.path.helper, helperName), {
-            _helper: helper,
-            _data: data,
-            schema: context.schema,
-        }).replace(/\n\s*\n/g, '\n');
+        try {
+            return renderer.render(path.join(context.path.helper, helperName), {
+                _helper: helper,
+                _data: data,
+                schema: context.schema,
+            }).replace(/\n\s*\n/g, '\n');
+        } catch (ex) {
+            throw error.rethrow.from(ex).original().message(`Problem rendering helper "${helperName}"`);
+        }
     };
     const isArr = (val: any) => {
         return Array.isArray(val);

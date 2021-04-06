b784a2f38cf7116902464d024944a3dbeb291d18
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-04-06 10:54:59 +0700

    remove logical compare

--- packages/logicalCompare/src/yamlEval.ts
+++
@@ -1,5 +0,0 @@
-import yaml = require('js-yaml');
-import detectResolve from './detectResolve';
-export default() => async (data, condition: string) => {
-    return await detectResolve()(data, yaml.load(condition));
-};
--- packages/logicalCompare/src/yamlEvalArr.ts
+++
@@ -1,5 +0,0 @@
-import yaml = require('js-yaml');
-import jsonEvalArr from './jsonEvalArr';
-export default () => async (data: any[], condition: string) => {
-    return await jsonEvalArr()(data, yaml.load(condition));
-};

2ab5a782949ce6668ecfd2565313a9c057d6d265
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-10-13 12:16:18 +0700

    better manager

--- packages/scaffolder/src/baseEntity/manager.ts
+++ packages/scaffolder/src/baseEntity/manager.ts
@@ -1,19 +1,28 @@
 import { BaseEntityModel } from './baseEntityModel';
 import { BaseEntity, BaseEntityModel as BaseEntityModelType } from '../types';
-class Manager {
+export interface Manager {
+    addEntity: (baseEntity: BaseEntity) => void,
+    addModel: (baseEntity: BaseEntityModelType) => void,
+    getModel: (baseEntityName: string) => BaseEntityModelType,
+    getModels: (baseEntityName: string) => BaseEntityModelType[],
+};
+class ManagerService implements Manager {
     models: BaseEntityModelType[] = [];
     modelKey: any = {};
-    async addEntity(baseEntity: BaseEntity) {
+    addEntity(baseEntity: BaseEntity) {
         let obj = new BaseEntityModel(baseEntity);
         this.models.push(obj);
         this.modelKey[baseEntity.name] = obj;
     }
-    async addModel(baseEtityModel: BaseEntityModelType) {
+    addModel(baseEtityModel: BaseEntityModelType) {
         this.models.push(baseEtityModel);
         this.modelKey[baseEtityModel.entity().name] = baseEtityModel;
     }
-    async getModel(baseEntityName: string) {
+    getModel(baseEntityName: string) {
         return this.modelKey[baseEntityName];
     }
+    getModels() {
+        return this.models;
+    }
 };
-export default Manager;
+export default ManagerService;

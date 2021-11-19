2d1f5ed32884b749891a579dd24df63af83a74ff
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2021-11-19 10:44:57 +0700

    dev

--- packages/scaffolder/test/dataGenerator/generateIDMap/BaseEntityManager.ts
+++ packages/scaffolder/test/dataGenerator/generateIDMap/BaseEntityManager.ts
@@ -1,9 +1,11 @@
 import manager from '../../../src/baseEntity/manager';
 import BaseEntityModelSingle from './BaseEntityModelSingle';
+import BaseEntityModelNumber from './BaseEntityModelNumber';
 import BaseEntityModelRowCount from './BaseEntityModelRowCount';
 
 let managerInstance = new manager();
 managerInstance.addModel(BaseEntityModelSingle);
 managerInstance.addModel(BaseEntityModelRowCount);
+managerInstance.addModel(BaseEntityModelNumber);
 
 export default managerInstance;
--- packages/scaffolder/test/dataGenerator/generateIDMap/generate.spec.ts
+++ packages/scaffolder/test/dataGenerator/generateIDMap/generate.spec.ts
@@ -8,8 +8,12 @@ import { generateIDMap } from '../../../src/dataGenerator/generateIDMap';
 mocha.describe('generateIDMap', function () {
     mocha.it('generate', async function () {
         let generated = generateIDMap(BaseEntityManager);
+
         assert.equal(10, generated.MyModel.data.length);
         assert.equal(30, generated.MyModelRowCount.data.length);
+        assert.equal(12, generated.MyModelNumber.data.length);
         assert.equal(36, generated.MyModel.data[0].MyProp1.length); // is uuid
+        assert.equal("number", typeof generated.MyModelNumber.data[0].MyProp1);
+        assert.equal(6, generated.MyModelNumber.data[5].MyProp1);
     });
 });
